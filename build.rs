6ce2014131b3e03f3541f714212b6a98f78a4223
jli <coderguy50@gmail.com>
2023-05-31 18:46:10 -0500

    added icon to executable

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
+#[cfg(windows)]
+extern crate winres;
+
 fn main() {
     #[cfg(target_os = "linux")]
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+    #[cfg(windows)]
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("res/icon.ico");
+    res.compile().unwrap();
 }
